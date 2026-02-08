22244b271f4f444006871e97881a337c6a536949
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-02-08 18:09:50 +0300

    feat(repo): add base repository

---
+++ app/base_repository.py
@@ -0,0 +1,53 @@
+from app.dependencies.database import BaseModel
+from sqlalchemy import delete, insert, select, update
+from sqlalchemy.orm import Session
+
+
+class SQLAlchemyRepository:
+    model: BaseModel
+    # TODO: replace commit to UoF
+
+    def __init__(self, session: Session):
+        self.db: Session = session
+
+    def add_one(self, entity):
+        stmt = insert(self.model).values(**entity).returning(self.model.id)
+        result = self.db.execute(stmt)
+        answer = result.scalar_one_or_none()
+        self.db.commit()
+        return answer
+
+    def find_all(self):
+        stmt = select(self.model)  # TODO: Добавить фильтрацию и пагинацию и сортировку
+        result = self.db.execute(stmt)
+        answer = result.scalars().all()
+        return answer
+
+    def find_one(self, entity_id):
+        stmt = select(self.model).where(self.model.id == entity_id)
+        result = self.db.execute(stmt)
+        answer = result.scalar_one_or_none()
+        return answer
+
+    def edit_one(self, entity_id, entity):
+        stmt = (
+            update(self.model)
+            .where(self.model.id == entity_id)
+            .values(**entity)
+            .returning(self.model.id)
+        )
+        result = self.db.execute(stmt)
+        answer = result.scalar_one_or_none()
+        self.db.commit()
+        return answer
+
+    def delete_one(self, entity_id):
+        stmt = (
+            delete(self.model)
+            .where(self.model.id == entity_id)
+            .returning(self.model.id)
+        )
+        result = self.db.execute(stmt)
+        answer = result.scalar_one_or_none()
+        self.db.commit()
+        return answer
