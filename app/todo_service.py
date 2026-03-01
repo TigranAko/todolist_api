0b4d0f2e243ce873d2ce5dd4f27954e5cda8d0ce
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-03-01 19:33:37 +0300

    feat(uow,service): use UoW in UserService and TodoService

--- app/todo_service.py
+++ app/todo_service.py
@@ -1,33 +1,30 @@
 from fastapi import Depends
-from sqlalchemy.orm import Session
 
-from database import get_db
-from todo_repository import TodoRepository
+from uow import UnitOfWork, get_uow
 
 
 class TodoService:
-    def __init__(self, session: Session):
-        self.session = session
-        self.repo = TodoRepository(session)
+    def __init__(self, uow: UnitOfWork):
+        self.uow = uow
 
     async def get_all_todos(self):
         """Список всех задач (для админа)"""
-        result = await self.repo.find_all()
+        result = await self.uow.todos.find_all()
         return result
 
     async def get_user_todos(self, user_id: int):
         """Список задач пользователя"""
-        result = await self.repo.find_user_todos(user_id)
+        result = await self.uow.todos.find_user_todos(user_id)
         return result
 
     async def get_todo(self, todo_id: int):
         """Получить задачу"""
-        result = await self.repo.find_one(todo_id)
+        result = await self.uow.todos.find_one(todo_id)
         return result
 
     async def create_todo(self, todo):
         """Создать задачу"""
-        result = await self.repo.add_one(todo.model_dump())
+        result = await self.uow.todos.add_one(todo.model_dump())
         return result
 
     async def create_todos(self, todos):
@@ -35,19 +32,19 @@ class TodoService:
         # TODO: need optimization
         result = []
         for todo in todos:
-            await result.append(await self.repo.add_one(todo.model_dump()))
+            await result.append(await self.uow.todos.add_one(todo.model_dump()))
         return result
 
     async def update_todo(self, todo_id, todo):
         """Создать задачу"""
-        result = await self.repo.edit_one(todo_id, todo)
+        result = await self.uow.todos.edit_one(todo_id, todo)
         return result
 
     async def delete_todo(self, todo_id):
         """Создать задачу"""
-        result = await self.repo.delete_one(todo_id)
+        result = await self.uow.todos.delete_one(todo_id)
         return result
 
 
-async def get_todo_service(session: Session = Depends(get_db)):
-    return TodoService(session)
+async def get_todo_service(uow: UnitOfWork = Depends(get_uow)):
+    return TodoService(uow)
--- app/user_service.py
+++ app/user_service.py
@@ -1,24 +1,21 @@
 from fastapi import Depends, HTTPException
-from sqlalchemy.orm import Session
 
-from database import get_db
-from user_repository import UserRepository
+from uow import UnitOfWork, get_uow
 from user_schemas import UserCreate
 
 
 class UserService:
-    def __init__(self, session: Session):
-        self.session = session
-        self.repo = UserRepository(session)
+    def __init__(self, uow: UnitOfWork):
+        self.uow = uow
 
     async def register(self, user: UserCreate):
-        if await self.repo.is_have_username(user.username):
+        if await self.uow.users.is_have_username(user.username):
             # TODO: change status code
             # TODO: split app exceptions and http exceptions
             raise HTTPException(
                 400, detail="Пользователь с таким именем уже существует"
             )
-        result = await self.repo.add_one(user.model_dump())
+        result = await self.uow.users.add_one(user.model_dump())
         return result
 
     async def login(): ...
@@ -26,19 +23,20 @@ class UserService:
     async def logout(): ...
 
     async def get_all_users(self):
-        result = await self.repo.find_all()
+        result = await self.uow.users.find_all()
         return result
 
     async def about(self, user_id):
-        result = await self.repo.find_one(user_id)
+        result = await self.uow.users.find_one(user_id)
         return result
 
     async def update(): ...
 
     async def delete(self, user_id):
-        result = await self.repo.delete_one(user_id)
+        result = await self.uow.users.delete_one(user_id)
+        # TODO: Можно удалять задачи вручную
         return result
 
 
-async def get_user_service(session: Session = Depends(get_db)):
-    return UserService(session)
+async def get_user_service(uow: UnitOfWork = Depends(get_uow)):
+    return UserService(uow)
