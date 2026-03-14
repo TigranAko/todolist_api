9f06c3d143e33380ad33c88d97140835e6eac3fe
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-03-14 10:17:54 +0300

    feat(env): add base ConfigDB

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 path_to_env = f"{Path.absolute().parent}/.env"
@@ -11,3 +12,11 @@ class BaseConfig(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore",
     )
+
+
+class ConfigDB(BaseConfig):
+    DB_URL: SecretStr
+
+
+config_db = ConfigDB()
+# пример использлвания config_db.DB_URL.get_secret_value()
