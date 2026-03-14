f63bf8ddde3184b242a34fb6cd52484133df9062
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-03-14 11:22:34 +0300

    feat(env): add ability use test, dev, prod db

--- app/config.py
+++ app/config.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from typing import Literal
 
-from pydantic import SecretStr
+from pydantic import SecretStr, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-path_to_env = f"{Path.absolute().parent}/.env"
+path_to_env = f"{Path().absolute().parent}/.env"
 
 
 class BaseConfig(BaseSettings):
-    model_confifg = SettingsConfigDict(
+    model_config = SettingsConfigDict(
         env_file=path_to_env,
         env_file_encoding="utf-8",
         extra="ignore",
@@ -15,7 +16,22 @@ class BaseConfig(BaseSettings):
 
 
 class ConfigDB(BaseConfig):
-    DB_URL: SecretStr
+    ENVIROMENT: Literal["TEST", "DEV", "PROD"]
+    TEST_DB_URL: SecretStr = "sqlite+aiosqlite:///:memory:"
+    DEV_DB_URL: SecretStr = "sqlite+aiosqlite:///dev.db"
+    PROD_DB_URL: SecretStr | None = None
+
+    @computed_field
+    def db_url(self) -> str:
+        match self.ENVIROMENT:
+            case "TEST":
+                return self.TEST_DB_URL
+            case "DEV":
+                return self.DEV_DB_URL
+            case "PROD":
+                if self.PROD_DB_URL is None:
+                    raise ValueError("Не удаось найти production базу данных")
+                return self.PROD_DB_URL
 
 
 config_db = ConfigDB()
