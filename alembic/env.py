8edc6e605c9988657ad01663982a1ecb8f71704b
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-03-28 21:49:17 +0300

    feat(migration): add metadata and db url

--- alembic/env.py
+++ alembic/env.py
@@ -18,14 +18,21 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.database import Base
+
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+from app.config import config_db
+from app.models import UserBase, ToDoBase
+
+print(UserBase, ToDoBase)
+
+config.set_main_option("sqlalchemy.url", config_db.db_url)
 
 
 def run_migrations_offline() -> None:
